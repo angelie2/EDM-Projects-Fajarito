724eebe0c260066ca9c7070c6fac0159da2bca57
Angelie Fajarito <afajarito24-0192@cca.edu.ph>
2025-04-23 18:52:56 +0800

    Add files via upload

--- rawfile/Finals Lab Task 3 Table Manipulation.sql
+++ rawfile/Finals Lab Task 3 Table Manipulation.sql
@@ -1,15 +1,28 @@
 CREATE DATABASE techdb;
 USE techdb;
 
--- Task 1
-CREATE TABLE products_tbl(
-id INT AUTO_INCREMENT PRIMARY KEY,
-product_name VARCHAR (100) NOT NULL,
-price DECIMAL (10,2)
--- Task 2
-CHECK (price>0)
+CREATE TABLE product_tbl (
+    id INT AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL,
+    price DECIMAL(10,2)
 );
-DESCRIBE products_tbl;
 
--- Task 4
-ALTER TABLE products_tbl MODIFY COLUMN product_name VARCHAR (120)
+DESCRIBE product_tbl;
+
+ALTER TABLE product_tbl
+ADD CONSTRAINT price_check CHECK (price > 0);
+
+INSERT INTO product_tbl (product_name, price) VALUES 
+ ('Laptop', 999.99),
+ ('Smartphone', 599.99),
+ ('Tablet', 299.99),
+ ('Keyboard', 19.99),
+ ('Mouse', 14.99),
+ ('Desk Lamp', 24.99),
+ ('Speakers', 9.99);
+
+ALTER TABLE product_tbl
+MODIFY COLUMN product_name VARCHAR(120) NOT NULL;
+
+SELECT * FROM product_tbl;
+
